d2fcb030b97f697770087a20f48c86fe7024c439
metinabadan <metinabadan@gmail.com>
2021-10-14 23:46:54 +0300

    added lane detection for video

--- main.py
+++ main.py
@@ -15,32 +15,69 @@ def requiredPartOfImage(image, vertices):
     maskedImage = cv2.bitwise_and(image,mask)
     return maskedImage
 
+def drawLines(image, lines):
+    image = np.copy(image)
+    blankImage = np.zeros((image.shape[0], image.shape[1],3),dtype=np.uint8)
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(blankImage,(x1,y1),(x2,y2),(0,255,0),thickness=10)
+
+    image = cv2.addWeighted(image, 0.8, blankImage, 1, 0.0)
+    return image
+
+"""
+dont need anymore after the processing video
+
 # load an image
 image = cv2.imread("road_lane.png")
 #showImage("original image",image)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+"""
 
-# shape[0]  height  -  shape[1] width  -  shape[2] color channel
-height = image.shape[0]
-width = image.shape[1]
+def process(image):
 
-# to get lane part of image
-verticesOfRequiredPartOfImage = \
-    [
-        (0,height),
-        (width/2,height/2),
-        (width,height)
-    ]
 
-grayImage = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
-#showImage("gray",grayImage)
+    # shape[0]  height  -  shape[1] width  -  shape[2] color channel
+    height = image.shape[0]
+    width = image.shape[1]
 
-cannyImage = cv2.Canny(grayImage, 100, 200)
-#showImage("canny",cannyImage)
+    # to get lane part of image
+    verticesOfRequiredPartOfImage = \
+        [
+            (0,height),
+            (width/2,height/2),
+            (width,height)
+        ]
 
-croppedImage = requiredPartOfImage(cannyImage,np.array([verticesOfRequiredPartOfImage],np.int32))
-#showImage("cropped",croppedImage)
+    grayImage = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    #showImage("gray",grayImage)
+
+    cannyImage = cv2.Canny(grayImage, 100, 120)
+    #showImage("canny",cannyImage)
+
+    croppedImage = requiredPartOfImage(cannyImage,np.array([verticesOfRequiredPartOfImage],np.int32))
+    #showImage("cropped",croppedImage)
+
+    lines = cv2.HoughLinesP(croppedImage,2,np.pi/60,160,np.array([]),40,100)
+
+    imageWithLines = drawLines(image,lines)
+
+    return imageWithLines
+
+cap = cv2.VideoCapture("Lane.mp4")
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    frame = process(frame)
+    cv2.imshow("frame",frame)
+    if (cv2.waitKey(1) & 0xFF == ord("q")):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 
 # to see coordinates of pixels
-plt.imshow(croppedImage)
-plt.show()
+#plt.imshow(imageWithLines)
+#plt.show()
